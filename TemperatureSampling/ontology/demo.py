804271b1f15aaaa61188e8c3e6b4cbe396264a9b
Markus <mabar16@student.sdu.dk>
2020-03-11 15:22:54 +0100

    Onthology 2.0

--- TemperatureSampling/ontology/demo.py
+++ TemperatureSampling/ontology/demo.py
@@ -2,60 +2,64 @@
 
 from wrapping import *
 
-TTL_FILENAME = './var/temperatureMapping.ttl'
+TTL_FILENAME = '.\\var\\temperatureMapping.ttl'
 
 g = model()
 print("initial file read")
 ###############################################################################
-################################################################# device ####
+################################################################### calibration ####
 
 device = N['/device']
 g.add((device, RDF.type, TYPES["Device"]))
 
 ###############################################################################
-################################################################### measurements ####
+################################################################# device ####
 
-# measurements = []
-# for measurementId in range(2): #Actually read these from excel file?
-#     measurement = N['device/measurements/%u' % measurementId]
-#     g.add((measurement, RDF.type, TYPES['TemperatureMeasurement']))
-#     g.add((device, RELATIONS.contains, measurement))
-#     measurements.append(measurement)
+sensor = N['/device/temperaturesensor']
+g.add((device, RELATIONS.has, sensor))
+
+###############################################################################
+################################################################### calibration ####
+
+calibration = N['/device/temperaturesensor/calibration']
+g.add((sensor, RELATIONS.has, calibration))
 
 ###############################################################################
 ############################################################# room mapping ####
 
-measurements = {
+distribution = {
     '0': {
-        'reading': 4095,
-        'time-before': '123456789012345',
-        'time-after': '123456789012346',
+        'min': '37',
+        'max': '37',
+        'avg': '37',
+        'start': '37',
+        'end': '37',
     },
     '1': {
-        'reading': 2095,
-        'time-before': '123456789012345',
-        'time-after': '123456789012346',
-    },
-    '2': {
-        'reading': 3095,
-        'time-before': '123456789012345',
-        'time-after': '123456789012346',
+        'min': '38',
+        'max': '38',
+        'avg': '38',
+        'start': '38',
+        'end': '38',
     },
 }
 
 ###############################################################################
 #################################################################### rooms ####
 
-measurementsMap = {}
-for rownumber in measurements:
-    data = measurements[rownumber]
-    measurement = N['device/measurements/%s' % rownumber]
-    g.add((measurement, RDF.type, TYPES['TemperatureMeasurement']))
-    g.add((measurement, ATTRIBUTES.label, Literal(rownumber)))
-    g.add((measurement, ATTRIBUTES.reading, Literal(data["reading"])))
-    g.add((measurement, ATTRIBUTES.timestamp, Literal(data["time-after"])))
-    g.add((device, RELATIONS.has, measurement))
-    measurementsMap[rownumber] = measurement
+distributionMap = {}
+for rownumber in distribution:
+    data = distribution[rownumber]
+    mapping = N['/device/temperaturesensor/calibration/mappings/%s' % rownumber]
+    g.add((mapping, RDF.type, TYPES['TemperatureDistribution']))
+    g.add((mapping, ATTRIBUTES.label, Literal(rownumber)))
+    g.add((mapping, ATTRIBUTES.min, Literal(data["min"])))
+    g.add((mapping, ATTRIBUTES.max, Literal(data["max"])))
+    g.add((mapping, ATTRIBUTES.avg, Literal(data["avg"])))
+    g.add((mapping, ATTRIBUTES.start, Literal(data["start"])))
+    g.add((mapping, ATTRIBUTES.end, Literal(data["end"])))
+    g.add((calibration, RELATIONS.has, mapping))
+    distributionMap[rownumber] = mapping
 
 
 ###############################################################################
@@ -72,11 +76,12 @@ print("parsed")
 
 q_dashboard = \
 '''
-SELECT DISTINCT ?measurement ?reading ?rowNumber
+SELECT DISTINCT ?min ?max ?calibration
 WHERE {
-    ?measurement     rdf:type types:TemperatureMeasurement .
-    ?measurement   attributes:label ?rowNumber .
-    ?measurement   attributes:reading ?reading .
+    ?calibration   rdf:type                types:TemperatureDistribution .
+    ?calibration   attributes:label        ?rowNumber .
+    ?calibration   attributes:min          ?min .
+    ?calibration   attributes:max          ?max .
         
 }
 '''
