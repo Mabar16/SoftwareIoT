990c90de849649455b4a23155f7bad6cf18dcf83
Victor <victor.s@live.dk>
2020-04-02 09:31:07 +0200

    cmts

--- TempOverWireless/socketServer/missingDataDetector.py
+++ TempOverWireless/socketServer/missingDataDetector.py
@@ -18,6 +18,7 @@ with open('logFromServer4.csv', 'r') as file_object:
                 temp = float(row[0])
                 lite = float(row[1])
                 cunt = int(row[2])
+                #Message count resets a few times throughout the data set
                 if cunt == 0 and not isFirst:
                     countsListList.append(counts)
                     counts = []
@@ -26,6 +27,12 @@ with open('logFromServer4.csv', 'r') as file_object:
                     counts.append(cunt)
 
     for countList in countsListList:
+        #If list length is equal to the final number added to the list, no messages are missing
         print("len: " + str(len(countList)) + " . Last num: " + str(countList[-1]))
+
+# len: 138786 . Last num: 138786
+# len: 12783 . Last num: 12783
+# len: 60456 . Last num: 60456
+# len: 88962 . Last num: 88962
             
          
