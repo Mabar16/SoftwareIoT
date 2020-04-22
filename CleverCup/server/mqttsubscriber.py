b04f8d970535a6cd74bc22e7152e5dcc572a442c
Victor <victor.s@live.dk>
2020-04-22 11:18:07 +0200

    use rds in server

--- CleverCup/server/mqttsubscriber.py
+++ CleverCup/server/mqttsubscriber.py
@@ -2,43 +2,29 @@ from __future__ import print_function # Python 2/3 compatibility
 import paho.mqtt.client as mqtt
 import time
 import json
-import boto3
+import postgresql
+import config
 
-dynamodb = boto3.resource('dynamodb',
-    region_name='eu-central-1',
-    aws_access_key_id='AKIAJLFH3N6PIXRQOY4Q',
-    aws_secret_access_key='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',
-)
+host="clevercupdb.ckz9vj0cvxvl.eu-central-1.rds.amazonaws.com"
+port=5432
+dbname="CleverCupDb"
+user="vibar"
+password=config.dbpassword
+
+
+db =postgresql.open('pq://'+user+':'+password+'@'+host+':'+str(port)'/'+dbname+'')
+
+# 
 def writeToTempTable(payload, timestamp):
-    table = dynamodb.Table('CleverCupTemperature')
     temp = payload['temperature']#.decode("utf-8")
     device = payload['deviceid']
-    table.put_item(
-        Item={
-            'id': timestamp,
-            'deviceid': device,
-            'temperature': str(temp)
+    timestamp = payload['pycomtime']
 
-        }
-    )
+    query = "Insert into temperature (\"value\", \"devicename\",\"pycomtime\") values  ("+temp+", '"+device+"', "+timestamp+")"
+    db.execute(query)
+
     print("TMPO")
 
-def writeToLocationTable(payload, timestamp):
-    
-    table = dynamodb.Table('CleverCupLocation')
-    lat = payload['latitude']#.decode("utf-8")
-    long = payload['longitude']#.decode("utf-8")
-    device = payload['deviceid']    
-    table.put_item(
-        Item={
-            'id': timestamp,
-            'device': device,
-            'lat': lat,
-            'long': long
-    
-        }
-    )
-    print("LOCO")
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -46,7 +32,6 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("clevercup/temperature")
-    client.subscribe("clevercup/geolocation")
 
 def decodeJson(payload):
     jsons = payload.decode("utf8")
@@ -59,9 +44,7 @@ def on_message(client, userdata, msg):
     if ('temperature' in msg.topic):
         payloadDict = decodeJson(msg.payload)
         writeToTempTable(payloadDict, timestamp)
-    if ('geolocation' in msg.topic):
-        payloadDict = decodeJson(msg.payload)
-        writeToLocationTable(payloadDict, timestamp)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
