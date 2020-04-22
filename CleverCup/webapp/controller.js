89d1b42fa928e8d42e340099f2b4a3be6328ac9e
Victor <victor.s@live.dk>
2020-04-22 15:34:07 +0200

    temp visuals

--- CleverCup/webapp/controller.js
+++ CleverCup/webapp/controller.js
@@ -3,80 +3,99 @@ const mqtt = require('mqtt') //npm install mqtt -g
 const app = express()
 const port = 3000
 
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+const { Client } = require('pg')
 
-// Set the region 
-AWS.config.update({region: 'eu-central-1'});
+var secret = require('./secret');
 
-var deviceLocations = 
-                    {
-                        'vicPycom':'',
-                        'markusPycom':''
-                    }
+const dbclient = new Client({
+    user: 'vibar',
+    host: 'clevercupdb.ckz9vj0cvxvl.eu-central-1.rds.amazonaws.com',
+    database: 'CleverCupDb',
+    password: secret.dbpassword(),
+    port: 5432,
+})
+dbclient.connect()
+
+var deviceLocations =
+{
+    'vicPycom': '',
+    'markusPycom': ''
+}
 app.use(express.static('static'))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/publishComfortRange', (req,res) =>{
-    
+app.get('/publishComfortRange', (req, res) => {
+
     publishComfortRange(req.query.deviceID, req.query.tempMin, req.query.tempMax)
     res.send("OK")
-} )
+})
 
-app.get('/getCupLocation', (req,res) =>{
-    
+app.get('/getDevices', (req,res) => res.send(Object.keys(deviceLocations)))
+
+app.get('/getCupLocation', (req, res) => {
+
     location = findCup(req.query.deviceID)
     console.log(location)
     res.send(location)
-} )
-    
+})
+
+app.get("/getAllTemperatureData", (requst,result)=>{
+    dbclient.query('select * from temperature where "devicename" = \''+requst.query.deviceID+'\' order by pycomtime', (err, res) => {
+        if(err)
+            console.log(err)
+        console.log(res.rowCount)
+        
+        result.send(res)
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-var client  = mqtt.connect("mqtt://3.126.242.230")
+var client = mqtt.connect("mqtt://3.127.128.243")
 
-client.on("connect", () =>{
+client.on("connect", () => {
     console.log("CONNEC")
     //
 })
 
 client.subscribe("device/vicPycom/location/update")
 
-client.on("message", (topic, message) =>{
-   // console.log(topic+ "    " + message)
-    if( topic.includes("location/update")){
+client.on("message", (topic, message) => {
+    // console.log(topic+ "    " + message)
+    if (topic.includes("location/update")) {
         payload = JSON.parse(message)
         devicename = topic.split("/")[1]
-        location = {lat: payload.latitude, lon: payload.longitude}
+        location = { lat: payload.latitude, lon: payload.longitude }
         deviceLocations[devicename] = location
     }
 })
 
-function publish(topic, message){
+function publish(topic, message) {
     console.log(topic)
     console.log(message)
     client.publish(topic, message, console.log)
 }
 
-function publishComfortRange(deviceID, tempMin, tempMax){
+function publishComfortRange(deviceID, tempMin, tempMax) {
     console.log("publishing! " + tempMin + "   " + tempMax)
 
-    topic = "device/"+deviceID+"/comfortrange/updates"
-    message = 
-                {
-                    'device': deviceID,
-                    'newmin': parseInt(tempMin, 10),
-                    'newmax': parseInt(tempMax, 10)
-                }
+    topic = "device/" + deviceID + "/comfortrange/updates"
+    message =
+    {
+        'device': deviceID,
+        'newmin': parseInt(tempMin, 10),
+        'newmax': parseInt(tempMax, 10)
+    }
     msgstring = JSON.stringify(message)
     publish(topic, msgstring)
 }
 
-function findCup(deviceID){    
+function findCup(deviceID) {
     return deviceLocations[deviceID]
 }
 
-function placeCupMarker(deviceID, lat, long){
-    L.marker([lat, long]).addTo(mymap).bindPopup("<b>"+deviceID+" is here!</b>.").openPopup();
+function placeCupMarker(deviceID, lat, long) {
+    L.marker([lat, long]).addTo(mymap).bindPopup("<b>" + deviceID + " is here!</b>.").openPopup();
 }
